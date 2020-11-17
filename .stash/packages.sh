169df06bc123333d931fa09c8dc4f89ed0add7a6
aghani@inahga.org <Ameer Ghani>
2020-11-17 14:59:58 -0600

    Add package lis

---
+++ .stash/packages.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+sudo dnf install -y \
+	remmina krita libreoffice golang g++ mozilla-ublock-origin mozilla-https-everywhere \
+	kakoune gnome-tweak-tool gnome-shell-extension-activities-configurator \
+	gnome-shell-extension-dash-to-dock chromium snapd
