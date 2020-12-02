65a652fcce8893ab84c75e8b2f06744ea3a4e10c
aghani@inahga.org <Ameer Ghani>
2020-12-01 21:15:08 -0600

    update packages.hs

--- .stash/packages.sh
+++ .stash/packages.sh
@@ -3,4 +3,5 @@
 sudo dnf install -y \
 	remmina krita libreoffice golang g++ mozilla-ublock-origin mozilla-https-everywhere \
 	kakoune gnome-tweak-tool gnome-shell-extension-activities-configurator \
-	gnome-shell-extension-dash-to-dock chromium snapd shellcheck rg moreutils
+	gnome-shell-extension-dash-to-dock chromium snapd ShellCheck moreutils gcc \
+	ripgrep podman ccls npm flex bison bear ansible clang-tools-extra
