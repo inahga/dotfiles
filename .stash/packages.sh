117919544a138d265dd809766347940d31df093c
Ameer Ghani <inahga@gmail.com>
2020-12-06 00:27:59 -0600

    update packages.sh

--- .stash/packages.sh
+++ .stash/packages.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 sudo dnf install -y \
-	remmina krita libreoffice golang g++ mozilla-ublock-origin mozilla-https-everywhere \
+	krita libreoffice golang g++ mozilla-ublock-origin mozilla-https-everywhere \
 	kakoune gnome-tweak-tool gnome-shell-extension-activities-configurator \
-	gnome-shell-extension-dash-to-dock chromium ShellCheck moreutils gcc \
-	ripgrep podman ccls npm flex bison bear ansible clang-tools-extra alacritty
+	gnome-shell-extension-dash-to-dock chromium ShellCheck moreutils gcc pass \
+	ripgrep podman ccls npm flex bison bear ansible clang-tools-extra alacritty \
+	git-crypt
