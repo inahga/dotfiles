77b8ae987da1aa45213144972c56b98ed74bb229
Ameer Ghani <57697428+inahga@users.noreply.github.com>
2020-12-15 22:53:32 -0600

    Update .install.sh

--- .install.sh
+++ .install.sh
@@ -3,4 +3,6 @@
 
 git clone --bare git@github.com:inahga/dotfiles.git "$HOME/.cfg"
 alias config='/usr/bin/git --git-dir=$HOME/.cfg/ --work-tree=$HOME'
+cd $HOME
+config checkout
 config config --local status.showUntrackedFiles no
