dbbf3194724f1d4a029ea178d047027f08215f39
Ameer Ghani <57697428+inahga@users.noreply.github.com>
2020-12-15 22:53:09 -0600

    Update .install.sh

--- .install.sh
+++ .install.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 # See https://www.atlassian.com/git/tutorials/dotfiles
 
-git clone --bare git@gitlab.inahga.org:aghani/dotfiles.git "$HOME/.cfg"
+git clone --bare git@github.com:inahga/dotfiles.git "$HOME/.cfg"
+alias config='/usr/bin/git --git-dir=$HOME/.cfg/ --work-tree=$HOME'
+config config --local status.showUntrackedFiles no
