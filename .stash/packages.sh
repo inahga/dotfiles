7617f42fd8b59542a3acc537f711dea9e867de87
Ameer Ghani <inahga@gmail.com>
2021-05-09 22:17:39 -0500

    these extensions aren't packaged by fedora anymore

--- .stash/packages.sh
+++ .stash/packages.sh
@@ -2,8 +2,7 @@
 
 sudo dnf install -y \
 	krita libreoffice golang g++ mozilla-ublock-origin mozilla-https-everywhere \
-	kakoune gnome-tweak-tool gnome-shell-extension-activities-configurator \
-	gnome-shell-extension-dash-to-dock chromium ShellCheck moreutils gcc pass \
+	kakoune gnome-tweak-tool ShellCheck moreutils gcc pass \
 	ripgrep podman ccls npm flex bison bear ansible clang-tools-extra alacritty \
 	git-crypt entr cppcheck pylint python3-language-server ImageMagick libvirt-client \
 	virt-manager fd-find clang socat valgrind wl-clipboard aspell aspell-en
