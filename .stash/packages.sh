325ff201b643e8e3f271d0a9a57fa6421044ed05
Ameer Ghani <inahga@gmail.com>
2021-05-09 22:21:57 -0500

    add flatpak packages

--- .stash/packages.sh
+++ .stash/packages.sh
@@ -6,3 +6,6 @@ sudo dnf install -y \
 	ripgrep podman ccls npm flex bison bear ansible clang-tools-extra alacritty \
 	git-crypt entr cppcheck pylint python3-language-server ImageMagick libvirt-client \
 	virt-manager fd-find clang socat valgrind wl-clipboard aspell aspell-en
+
+flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
+flatpak install -y com.discordapp.Discord org.signal.Signal com.spotify.Client com.obsproject.Studio
