c860163d11cd7eade1d78b64162c3b521233dde8
Ameer Ghani <inahga@gmail.com>
2021-07-07 18:43:34 -0500

    stuff and things

--- .stash/packages.sh
+++ .stash/packages.sh
@@ -1,4 +1,6 @@
-#!/bin/bash
+#!/bin/bash -e
+
+sudo dnf erase -y gnome-shell-extension-background-logo
 
 sudo dnf install -y \
 	krita libreoffice golang g++ mozilla-ublock-origin mozilla-https-everywhere \
@@ -8,4 +10,10 @@ sudo dnf install -y \
 	virt-manager fd-find clang socat valgrind wl-clipboard aspell aspell-en
 
 flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
-flatpak install -y com.discordapp.Discord org.signal.Signal com.spotify.Client com.obsproject.Studio
+flatpak install -y com.discordapp.Discord org.signal.Signal com.spotify.Client com.obsproject.Studio \
+	com.valvesoftware.Steam
+
+pushd /tmp
+curl -LO https://github.com/kak-lsp/kak-lsp/releases/download/v10.0.0/kak-lsp-v10.0.0-x86_64-unknown-linux-musl.tar.gz
+tar xzvf kak-lsp*
+cp kak-lsp "${HOME}/.local/bin"
