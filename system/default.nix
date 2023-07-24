c2065eb889264aff39fdc8d7887d79a7eade3a68
Ameer Ghani <inahga@divviup.org>
2023-07-24 12:37:42 -0400

    nix: docker is borken

--- system/default.nix
+++ system/default.nix
@@ -5,7 +5,22 @@
   ...
 }: {
   nixpkgs.config.allowUnfree = true;
-  nixpkgs.overlays = [(import ../pkgs)];
+  nixpkgs.overlays = [
+    (import ../pkgs)
+
+    # pin docker to older nixpkgs: https://github.com/NixOS/nixpkgs/issues/244159
+    (let
+      pinnedPkgs = import (pkgs.fetchFromGitHub {
+        owner = "NixOS";
+        repo = "nixpkgs";
+        rev = "b6bbc53029a31f788ffed9ea2d459f0bb0f0fbfc";
+        sha256 = "sha256-JVFoTY3rs1uDHbh0llRb1BcTNx26fGSLSiPmjojT+KY=";
+      }) {};
+    in
+      final: prev: {
+        docker = pinnedPkgs.docker;
+      })
+  ];
 
   nix = {
     settings.auto-optimise-store = true;
